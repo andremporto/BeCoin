f6f5dd0d09beacdbfcc01c9e5a0e8d99068fc5d7
André Porto <andreporto@me.com>
2023-08-18 15:37:27 -0300

    Initial Commit

---
+++ BeCoin/BeCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeCoinApp.swift
+//  BeCoin
+//
+//  Created by André Porto on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BeCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeCoin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BeCoin
+//
+//  Created by André Porto on 18/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
