97b0b2a4ab55981df8e543048ab48982f4084a52
André Porto <andreporto@me.com>
2023-08-29 17:34:09 -0300

    Reload the list with updated data

---
+++ BeCoin/Utilities/HapticManager.swift
@@ -0,0 +1,19 @@
+//
+//  HapticManager.swift
+//  BeCoin
+//
+//  Created by André Porto on 29/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+    
+}
